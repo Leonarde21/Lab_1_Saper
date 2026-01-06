1c47891315fabe703d1305a9f16925e818691df8
Kirill Pelevin <kirill.pel76@gmail.com>
2026-01-07 00:29:40 +0300

    Убраны строки нужные для отладки

--- Saper/SaperMain.java
+++ Saper/SaperMain.java
@@ -39,71 +39,6 @@ public class SaperMain {
             return;
         }
 
-        String[][] exampleOne = {
-                {"bomb", "bomb", "1", "0"},
-                {"bomb",    "3",   "1",  "0"},
-                {"2",    "2",   "0",  "0"},
-                {"bomb",    "1",   "0",  "0"}
-        };
-        String[][] exampleTwo = {
-                {"0",    "0",   "1",     "1",    "1"},
-                {"0",    "1",   "2",     "bomb", "1"},
-                {"0",    "1",   "bomb",  "2",    "1"},
-                {"0",    "2",   "3",     "4",    "2"},
-                {"0",    "1",   "bomb",  "bomb", "bomb"}
-        };
-        String[][] exampleThree = {
-                {"bomb", "1", "1", "bomb", "1"},
-                {"1", "1", "1", "1", "1"},
-                {"0", "0", "0", "0", "0"},
-                {"0", "0", "0", "0", "0"},
-                {"0", "0", "0", "0", "0"}
-        };
-        String[][] exampleFour = {
-                {"1",    "bomb", "3",    "bomb", "2"},
-                {"1",    "2",    "4",    "bomb", "3"},
-                {"0",    "1",    "bomb", "4",    "bomb"},
-                {"1",    "2",    "2",    "bomb", "3"},
-                {"bomb", "1",    "1",    "2",    "bomb"}
-        };
-
-        String[][] exampleFive = {
-                {"1",     "bomb",  "2",     "1",     "0",     "1",     "1",     "2",     "1",     "2",     "2",     "bomb",  "2",     "bomb",  "bomb",  "1",     "0",     "0",     "1",     "bomb"},
-                {"2",     "3",     "bomb",  "1",     "1",     "2",     "bomb",  "2",     "bomb",  "3",     "bomb",  "4",     "3",     "3",     "2",     "1",     "0",     "1",     "2",     "2"},
-                {"1",     "bomb",  "2",     "2",     "3",     "bomb",  "3",     "2",     "1",     "3",     "bomb",  "3",     "bomb",  "1",     "0",     "0",     "1",     "3",     "bomb",  "2"},
-                {"1",     "1",     "1",     "1",     "bomb",  "bomb",  "2",     "0",     "0",     "1",     "1",     "2",     "1",     "2",     "1",     "1",     "1",     "bomb",  "bomb",  "2"},
-                {"0",     "1",     "2",     "3",     "3",     "2",     "2",     "2",     "2",     "1",     "0",     "0",     "0",     "1",     "bomb",  "1",     "1",     "2",     "2",     "1"},
-                {"0",     "1",     "bomb",  "bomb",  "2",     "1",     "1",     "bomb",  "bomb",  "2",     "0",     "0",     "0",     "1",     "1",     "1",     "0",     "0",     "0",     "0"},
-                {"0",     "2",     "3",     "4",     "bomb",  "1",     "1",     "3",     "bomb",  "2",     "1",     "2",     "2",     "1",     "0",     "0",     "1",     "2",     "2",     "1"},
-                {"0",     "1",     "bomb",  "3",     "3",     "3",     "2",     "2",     "2",     "3",     "3",     "bomb",  "bomb",  "2",     "0",     "1",     "3",     "bomb",  "bomb",  "1"},
-                {"0",     "1",     "2",     "bomb",  "2",     "bomb",  "bomb",  "3",     "3",     "bomb",  "bomb",  "6",     "bomb",  "3",     "0",     "1",     "bomb",  "bomb",  "4",     "2"},
-                {"2",     "3",     "3",     "3",     "3",     "4",     "bomb",  "3",     "bomb",  "bomb",  "4",     "bomb",  "bomb",  "2",     "0",     "1",     "2",     "3",     "bomb",  "1"},
-                {"bomb",  "bomb",  "bomb",  "3",     "bomb",  "2",     "2",     "3",     "3",     "2",     "3",     "3",     "3",     "1",     "0",     "0",     "0",     "1",     "2",     "2"},
-                {"4",     "6",     "5",     "bomb",  "2",     "1",     "1",     "bomb",  "2",     "1",     "1",     "bomb",  "1",     "0",     "1",     "1",     "1",     "0",     "1",     "bomb"},
-                {"bomb",  "bomb",  "bomb",  "4",     "2",     "0",     "1",     "2",     "bomb",  "1",     "1",     "1",     "1",     "0",     "1",     "bomb",  "1",     "0",     "1",     "1"},
-                {"3",     "5",     "bomb",  "bomb",  "1",     "0",     "0",     "1",     "1",     "1",     "0",     "0",     "0",     "0",     "1",     "1",     "1",     "0",     "0",     "0"},
-                {"bomb",  "2",     "2",     "2",     "2",     "2",     "2",     "1",     "1",     "1",     "1",     "0",     "0",     "0",     "0",     "0",     "0",     "0",     "0",     "0"},
-                {"2",     "2",     "0",     "0",     "1",     "bomb",  "bomb",  "1",     "2",     "bomb",  "2",     "0",     "1",     "1",     "1",     "0",     "0",     "0",     "0",     "0"},
-                {"bomb",  "1",     "0",     "0",     "1",     "2",     "2",     "1",     "2",     "bomb",  "3",     "1",     "2",     "bomb",  "3",     "1",     "1",     "0",     "0",     "0"},
-                {"3",     "3",     "1",     "0",     "1",     "1",     "1",     "0",     "1",     "2",     "bomb",  "2",     "3",     "bomb",  "3",     "bomb",  "1",     "0",     "0",     "0"},
-                {"bomb",  "bomb",  "2",     "0",     "1",     "bomb",  "2",     "2",     "3",     "3",     "4",     "bomb",  "4",     "3",     "4",     "2",     "1",     "0",     "0",     "0"},
-                {"3",     "bomb",  "2",     "0",     "1",     "1",     "2",     "bomb",  "bomb",  "bomb",  "3",     "bomb",  "3",     "bomb",  "bomb",  "1",     "0",     "0",     "0",     "0"}
-        };
-
-//        field = exampleFive;
-
-        int size = field.length;
-
-        for (int i = 0; i < size; i++) {
-            for (int j = 0; j < size; j++) {
-                if (field[i][j].equals("bomb"))
-                    System.out.print(field[i][j] + "\s\s");
-                else
-                    System.out.print(field[i][j] + "\s\s\s\s\s");
-            }
-            System.out.println();
-        }
-        System.out.println();
 
         SaperSolution2 solution = new SaperSolution2();
         String[][] newField = solution.Solutin(field, 0.2);
@@ -113,17 +48,7 @@ public class SaperMain {
             newField = solution.Solutin(field, 0.2);
             countTry++;
         }
-        System.out.println(countTry);
 
-        for (int i = 0; i < size; i++) {
-            for (int j = 0; j < size; j++) {
-                if (newField[i][j] == "flag")
-                    System.out.print(newField[i][j] + "\s\s");
-                else
-                    System.out.print(newField[i][j] + "\s\s\s\s\s");
-            }
-            System.out.println();
-        }
 
         SaperWorkFile saperWorkFile = new SaperWorkFile();
         saperWorkFile.WriteFile(newField);
